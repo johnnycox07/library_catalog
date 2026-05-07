321d7929b7c1651ded899fd532db4d7bbf1d103e
Anton <johnnycoxd@gmail.com>
2026-05-07 22:07:44 +0500

    alembic

--- src/library_catalog/core/database.py
+++ src/library_catalog/core/database.py
@@ -2,7 +2,7 @@ from typing import AsyncGenerator
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-from config import settings
+from library_catalog.core.config import settings
 
 
 class Base(DeclarativeBase):
